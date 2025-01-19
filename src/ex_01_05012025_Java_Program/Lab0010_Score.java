348bf2f55878b20c2aa395a467c4facf9ae30cfe
UtkalikaPanda <utkalikapanda89@gmail.com>
2025-01-19 18:29:49 +0530

    19th Jan Task2

---
+++ src/ex_01_05012025_Java_Program/Lab0010_Score.java
@@ -0,0 +1,15 @@
+package ex_01_05012025_Java_Program;
+
+public class Lab0010_Score {
+    public static void main(String[] args) {
+        int score = 1002;
+        String grade = (score>=90 && score<=100) ? "A Grade":
+                (score>=80 && score<=89) ? "B grade":
+                        (score>=70 && score<=79) ? "C Grade":
+                                (score>=60 && score<=69) ? "D Grade":
+                                        (score>=0 && score<=59) ? "F Grade": "Invalid score";
+        System.out.println(grade);
+
+
+    }
+}
