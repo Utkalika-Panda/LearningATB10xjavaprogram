662fdca8b059c25452ac869820a31cd8233bd3a1
UtkalikaPanda <utkalikapanda89@gmail.com>
2025-01-19 18:31:24 +0530

    19th Jan Task2

--- src/ex_01_05012025_Java_Program/Lab0010_Score.java
+++ src/ex_01_05012025_Java_Program/Lab0010_Score.java
@@ -2,7 +2,7 @@ package ex_01_05012025_Java_Program;
 
 public class Lab0010_Score {
     public static void main(String[] args) {
-        int score = 1002;
+        int score = 2;
         String grade = (score>=90 && score<=100) ? "A Grade":
                 (score>=80 && score<=89) ? "B grade":
                         (score>=70 && score<=79) ? "C Grade":
