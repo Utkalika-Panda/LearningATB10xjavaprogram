41edc6e9a02a3f4afb80360f4bb110dc5d23d8d7
UtkalikaPanda <utkalikapanda89@gmail.com>
2025-02-07 19:47:28 +0530

    6th feb

---
+++ src/ex_string_function/ex_6thfeb_string.java
@@ -0,0 +1,33 @@
+package ex_string_function;
+
+public class ex_6thfeb_string {
+    public static void main(String[] args) {
+       // String s1="  UTKALIKA PANDA  ";
+       // String s2="panda";
+       // System.out.println(s1.charAt(3));//index of each character
+        //System.out.println(s1.charAt(12));
+        //System.out.println(s1.concat(s2));//join 2 string
+        //System.out.println(s1.contains("utk"));
+       //System.out.println(s1.equals("UTKALIKA"));
+        //System.out.println(s1.equalsIgnoreCase("UTKALIKA"));
+       // System.out.println(s1.indexOf("k"));
+        //System.out.println(s1.length());
+        //System.out.println(s1.replace('a',','));
+        //System.out.println(s1.split(",");
+        //System.out.println(s1.substring(4));
+        //System.out.println(s1.toLowerCase());
+        //System.out.println(s1.toUpperCase());
+        //System.out.println(s1.isEmpty());
+        //System.out.println(s1.replaceAll("PANDA","Naman"));
+        //System.out.println(s1.startsWith("K"));
+        //System.out.println(s1.endsWith("A"));
+        //System.out.println(s1.trim());
+        //System.out.println(s1.repeat(2));
+        /*StringBuilder sb= new StringBuilder("Utkalika");
+        sb.append("Panda");
+        System.out.println(sb);*/
+        StringBuilder sb = new StringBuilder("Hi");
+        sb.append("How are you");
+        System.out.println(sb.toString());
+    }
+}
