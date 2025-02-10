56320a383d001af7bcb1819d5b064763e1e50099
UtkalikaPanda <utkalikapanda89@gmail.com>
2025-02-10 20:02:49 +0530

    7th feb

---
+++ src/Task_7thfeb/Left_angled_triangle.java
@@ -0,0 +1,13 @@
+package Task_7thfeb;
+
+public class Left_angled_triangle {
+    public static void main(String[] args){
+        int n=5;
+        for (int i=n;i>=1;i--){
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
